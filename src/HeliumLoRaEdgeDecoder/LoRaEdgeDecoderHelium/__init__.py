da82dfa02c1d89fcde0fdf37a6c68e7bfd3abe00
Oscar Naim <62676340+onaim-msft@users.noreply.github.com>
2022-05-02 21:41:53 -0500

    Update __init__.py
    Removed mslib reference from code

--- src/HeliumLoRaEdgeDecoder/LoRaEdgeDecoderHelium/__init__.py
+++ src/HeliumLoRaEdgeDecoder/LoRaEdgeDecoderHelium/__init__.py
@@ -47,7 +47,6 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from msilib.schema import Error
 from typing import List
 import logging
 
